815519b805c99d972e6a217a158507ec8249bc46
eslam emam <Eslam.Emam@microtec.com.eg>
2025-10-31 21:54:17 +0200

    add new feature

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
-
+  age = 30;
+  name = 'abdelrahman mohamed';
+  isTrue = false;
+  names = ['ahmed', 'mohamed', 'sayed', 'ali'];
+  car = 'ww';
+  isActive = false;
+  isError = true;
 }
